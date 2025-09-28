6b1a551f9b6c29db213a783869823752e278afa7
Your Name <your@email.com>
2025-09-28 17:37:07 +0700

    Add account association credentials

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,6 +1,11 @@
 const ROOT_URL = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
 
 export const minikitConfig = {
+  accountAssociation: {
+    "header": "eyJmaWQiOjEzNTQyNDksInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhiOWJkZjNFNUE0RkNEQjA2N2Q3RDU1MjY0ZGM1NEE2MUJhMDc0RDIzIn0",
+    "payload": "eyJkb21haW4iOiJ3b3JrLWZ1bi52ZXJjZWwuYXBwIn0",
+    "signature": "MHhmOWIxMDQ4NTA0YmYwYzY1NjVhNGIxYmQ4MTQwN2VjYWU4N2RlYWZhYTEyMWY3MDA2ZDg4Y2Y1MjU5NGYzMDAwN2NmMDhhOWI4MGFhYTg1OTI3ODA5YWQxMjdkODE2ZGI0YzQ5MGMxMmU2ZGVlZTQ3YzU0MmVkNTg1MzBkNmFlYjFi"
+  },
   miniapp: {
     version: "1",
     name: "WORK.FUN", 
